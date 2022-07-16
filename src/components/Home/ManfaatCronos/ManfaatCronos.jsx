056127faaa3b50003ea020bdda6cd1dce72c5111
chamida12 <khamidaumami@gmail.com>
2022-07-17 00:04:27 +0700

    mengatur tinggi card file manfaat cronos

--- src/components/Home/ManfaatCronos/ManfaatCronos.jsx
+++ src/components/Home/ManfaatCronos/ManfaatCronos.jsx
@@ -3,13 +3,16 @@ import React from 'react';
 const { Meta } = Card;
 
 const ManfaatCronos = () => (
-<div style={{background:"#F0F8FF"}}>
-    <div style={{textAlign:"center"}}>
-        <p>Membantu Pengusaha Memajukan Perusahaan Dengan Sistem</p>
+<div style={{background:"#F0F8FF"}}> 
+{/* #ececec */}
+    <div style={{height:"100vh",textAlign:"center",padding:"50px",fontFamily:"sans-serif"
+    ,fontSize:"20px"}}>
+        <p>Membantu Pengusaha Memajukan</p>
+        <p style={{lineHeight:"3px"}}> Perusahaan Dengan Sistem</p>
     </div>
-    <div style={{}}>
+    <div style={{margin:"180px"}}>
 
-        <Row gutter={16} >
+        <Row gutter={20} >
             <Col span={8}>
                 <Card
                     hoverable
