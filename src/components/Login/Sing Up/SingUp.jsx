5efe224e790f3ae6ad253668657f0e01a30a3802
chamida12 <khamidaumami@gmail.com>
2022-07-20 16:58:37 +0700

    3/4 % bagian sing up lengkapi data diri

--- src/components/Login/Sing Up/SingUp.jsx
+++ src/components/Login/Sing Up/SingUp.jsx
@@ -1,5 +1,6 @@
-import { Card, Input} from 'antd';
+import { Card, Input, Space} from 'antd';
 import React from 'react';
+import { InfoCircleTwoTone } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
 import "./SingUp.css";
 
@@ -39,8 +40,48 @@ const SingUp =()=> {
                   
                     <Input placeholder='Negara' style={{width:"315px"}}></Input>
                     <Input placeholder='Negara Bagian' style={{width:"315px",insetInline:"85px"}}></Input>
-              
+                    <Input placeholder='Nama Organisasi' type={"text"} style={{marginTop:"15px"}}/>
+                    <Input placeholder='Kota' style={{width:"315px",marginTop:"15px"}}></Input>
+                    <Input placeholder='Kode Pos' style={{width:"315px",insetInline:"85px",marginTop:"15px"}}></Input>
                 </div>
+                <div style={{margin:"30px"}}>
+                    <p style={{color:"gray"}}>
+                        PENGATURAN REGIONAL
+                    </p>
+                </div>
+                <div style={{margin:"30px",lineHeight:"0px"}}>
+                   <tg>Mata Uang *</tg>
+                        <div style={{marginLeft:"400px"}}>
+
+                            <tg>Bahasa *</tg>
+                        </div>
+                </div>
+                <div style={{margin:"30px"}}>
+                    <Input placeholder='Mata Uang' style={{width:"315px"}}></Input>
+                    <Input type={"language"} placeholder='Bahasa' style={{width:"315px",insetInline:"85px"}}></Input>
+                </div>
+                <div style={{margin:"30px"}}>
+                    <p>
+                        Zona Waktu *
+                    </p>
+                    <Input placeholder='Nama Organisasi' type={"time"}/>
+                </div>
+                <div style={{margin:"30px"}}>
+                    <p style={{color:"gray"}}>
+                        PENGATURAN PAJAK
+                    </p>
+                    <Space>
+                        <InfoCircleTwoTone />
+                            <tg>
+                                Konfigurasi tarif pajak di organisasi anda dengan melakukan navigasi ke pengaturan > pajak.
+                            </tg> 
+                    </Space>
+                </div>
+                <div style={{margin:"30px"}}>
+                    <p> Catatan: </p>
+                    <p style={{color:"GrayText",lineHeight:"2px",textIndent:"35px"}}> anda dapat mengubah semua detail ini  nantinya di pengaturan, jika dibutuhkan. </p>
+                </div>
+              
 
                     
 
