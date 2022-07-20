62d11e7f088a536b1e78fe1da8156c5e344a41d6
chamida12 <khamidaumami@gmail.com>
2022-07-20 12:22:07 +0700

    sing Up

--- src/components/Login/Sing Up/SingUp.jsx
+++ src/components/Login/Sing Up/SingUp.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Card } from 'antd';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import "./SingUp.css";
 
 function SingUp() {
     const navigate = useNavigate()
   return (
-    <div style={{weigth:"100vh",background:"blue"}}>
-      
+    <div className='bg'>
+        <div style={{padding:"80px"}}>
+            <p style={{textAlign:"center",fontSize:"18px",lineHeight:"15px",fontWeight:"normal"}}>
+                Finansial Acounting
+            </p>
+            <p style={{textAlign:"center",lineHeight:"5px",fontSize:"18px",fontWeight:"normal"}}>
+                Finansial Acounting adalah perangkat lunak akutansi online yang tuntas
+            </p>   
+            <Card style={{width:"800px",margin:"auto",height:"1000px"}} >
+                <p style={{textAlign:"center",padding:"20px",fontSize:"25px",fontFamily:"fantasy"}}>
+                        Konfigurasi Profil Organisasi Anda
+                </p>
+                <div  style={{margin:"30px"}}>
+                    <p style={{color:"gray"}}>
+                        DETAIL ORGANISASI
+                    </p>
+                    <p>
+                        Nama Organisasi *
+                    </p>
+
+                </div>
+                 
+            </Card>
+        </div>
     </div>
   )
 }
