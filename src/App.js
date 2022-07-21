91f3d90acd88f09842f6cc1faeb5b2873ee0bfdf
chamida12 <khamidaumami@gmail.com>
2022-07-21 14:29:47 +0700

    sing Up beres sing In beres Kelengkapan Data beres

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from '../src/components/Home/Home.jsx';
 import PriceList from '../src/components/PriceList/PriceList'
 import SingUp from './components/Login/Sing Up/SingUp';
 import BuatPassword from './components/Login/Sing Up/BuatPassword';
+import SingIn from './components/Login/Sing In/SingIn';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/PriceList" element={<PriceList />} />
           <Route path="/kelengkapanData" element={<SingUp/>}/>
           <Route path='/signUp' element={<BuatPassword/>}/>
+          <Route path='signIn' element={<SingIn/>}/>
         </Routes>
       </Router>
     </div>
--- src/components/Login/Sing Up/BuatPassword.jsx
+++ src/components/Login/Sing Up/BuatPassword.jsx
@@ -1,8 +1,48 @@
+import { Card, Checkbox, Input, Layout,Button } from 'antd'
+import { LockTwoTone,MailTwoTone } from '@ant-design/icons'
+import { Content, Header } from 'antd/lib/layout/layout'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const BuatPassword =()=> {
+  const navigate = useNavigate()
   return (
-    <div>BuatPassword</div>
+    <div style={{background: "#F0F8FF",height: "100vh"}}>
+        <Layout>
+            <Header style={{background:"#4682B4"}}>
+                <p style={{marginLeft:"900px"}}>Already have a Finansial Acounting Acount?
+                <a href='/signIn' style={{color:'Highlight'}}> Sign In</a>
+                </p> 
+            </Header>
+            <Content>
+              <div style={{padding:"30px",background: "#F0F8FF"}}>
+                <Card style={{width:"700px",margin:"auto",height:"500px",padding:"30px",boxShadow:" 1px 1px 3px grey"}}>
+                  <div style={{padding:"15px"}}>
+                  <p style={{textAlign:"center",fontSize:"20px"}}> Experience Finansial Acounting for FREE</p>
+
+                  <Input type={"email"} placeholder="Email" style={{height:"50px"}} 
+                  prefix={<MailTwoTone   
+                  style={{fontSize:"20px",textIndent:"4px"}}/>} />
+                  <br/>
+                  <br/>
+                  <Input type={"password"} placeholder="Password" style={{height:"50px"}} 
+                  prefix={<LockTwoTone 
+                  style={{fontSize:"20px",textIndent:"4px"}}/>} />
+                  </div>
+                  <div style={{margin:"20px"}}>
+                  <p style={{lineHeight:""}}> Your data will be in US data center</p>
+                  <Checkbox style={{lineHeight:"2px"}}> I agree to the Terms of Service and Privacy Policy</Checkbox>
+                  </div>
+                  <Button style={{margin:"15px",width:"580px",height:'50px'}} type="primary" onClick={()=> {navigate("/kelengkapanData")}}>
+                    CREATE ACCOUNT
+                  </Button>
+                </Card>
+              </div>
+
+            </Content>
+        </Layout>
+
+    </div>
   )
 }
 
