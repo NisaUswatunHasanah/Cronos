696385ca60fc74a9e566bd86782c4ada96371830
chamida12 <khamidaumami@gmail.com>
2022-07-28 16:59:49 +0700

    modif validasi form password

--- src/components/Login/Sing Up/BuatPassword.jsx
+++ src/components/Login/Sing Up/BuatPassword.jsx
@@ -1,12 +1,25 @@
 import { Card, Checkbox, Input, Layout,Button } from 'antd'
 import { LockTwoTone,MailTwoTone } from '@ant-design/icons'
 import { Content, Header } from 'antd/lib/layout/layout'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Logo from "../../../Assets/cronos.png"
 import { useNavigate } from 'react-router-dom'
+import { postUserPassword,getUser } from '../../services/authServices'
 
 const BuatPassword =()=> {
   const navigate = useNavigate()
+  const setListUser = useState([]);
+  const [massage,setMessage]= useState("");
+  const setIsEditing = useState(false);
+  const [dataUser,setDataUser] = useState({email:"",password:""});
+  console.log(dataUser,setDataUser);
+  
+  useEffect(()=>{
+      getUser(setListUser);
+      setTimeout(()=>{
+          setMessage("");
+      },3000);
+  },[massage]);
   return (
     <div style={{background: "#F0F8FF",height: "100vh"}}>
         <Layout>
@@ -26,19 +39,28 @@ const BuatPassword =()=> {
 
                   <Input type={"email"} placeholder="Email" style={{height:"50px"}} 
                   prefix={<MailTwoTone   
-                  style={{fontSize:"20px",textIndent:"4px"}}/>} />
+                  style={{fontSize:"20px",textIndent:"4px"}}/>}
+                  value={dataUser.email} 
+                                onChange={(e)=>{
+                                    setDataUser({...dataUser,email:e.target.value})
+                                }} />
                   <br/>
                   <br/>
                   <Input.Password type={"password"} placeholder="Password" style={{height:"50px"}} 
                   prefix={<LockTwoTone 
-                  style={{fontSize:"20px",textIndent:"4px"}}/>} 
+                  style={{fontSize:"20px",textIndent:"4px"}}
+                  value={dataUser.password} 
+                                onChange={(e)=>{
+                                    setDataUser({...dataUser,password:e.target.value})
+                                }} />} 
                   />
                   </div>
                   <div style={{margin:"20px"}}>
                   <p style={{lineHeight:""}}> Your data will be in US data center</p>
                   <Checkbox style={{lineHeight:"2px"}}> I agree to the Terms of Service and Privacy Policy</Checkbox>
                   </div>
-                  <Button style={{margin:"15px",width:"580px",height:'50px'}} type="primary" onClick={()=> {navigate("/kelengkapanData")}}>
+                  <Button style={{margin:"15px",width:"580px",height:'50px'}} type="primary"onClick={()=>{postUserPassword(dataUser,setDataUser,setMessage,setIsEditing)
+                        navigate("/kelengkapanData")}}>
                     CREATE ACCOUNT
                   </Button>
                 </Card>
