008ffafe56b003f2d0c4cb18efa6cfb060831964
NisaUswatunHasanah <nisaneyzo@gmail.com>
2022-07-29 14:35:36 +0700

    routing feature

--- src/components/Features/Purchasing.jsx
+++ src/components/Features/Purchasing.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import styled from "styled-components";
 
 const Purchasing = () => {
   return (
-    <div>Purchasing</div>
+    <div>
+    {/* <Wrapper> */}
+      <CardContent>
+        {/* <CardPrev>
+          <CH6>Course</CH6>
+          <CH3>JavaScript Fundamentals</CH3>
+        </CardPrev> */}
+        <CardInfo>
+          <h6>Chapter 4</h6>
+          <h2>Callbacks Yosh & Closures</h2>
+          <ButtonCard>INFO</ButtonCard>
+        </CardInfo>
+      </CardContent>
+    {/* </Wrapper> */}
+  </div>
   )
 }
 
